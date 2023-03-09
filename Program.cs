8a45c6fb84a95a4a82530f8009acb05985792c63
sazurov <102252019+sazurov@users.noreply.github.com>
2023-03-09 21:27:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
-﻿int[] mas = new int[7] 
+int[] mas = new int[7]
 { 3600, 2100, 1000, 1450, 2390, 9100, 420};
-
 Console.WriteLine("Введите сумму,которую необходимо представить в количестве купюр n номинала.");
 
 int n = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i < 1; i++)
-{
-   foreach (int x in mas) Console.WriteLine($"{n/x} Купюры номиналом {x}");
-}
-
+foreach (int x in mas) Console.WriteLine($"{n / x} Купюры номиналом {x}");
 
 
 
